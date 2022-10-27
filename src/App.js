c85d97c91d0969f7a58d3685af0333acad57182c
hildelio <hildelio@gmail.com>
2022-10-27 17:12:59 -0300

    Consertando requisito 4 quebrado

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends React.Component {
     cardAttr3: '',
     cardImage: '',
     cardRare: '',
-    cardTrunfo: 'false',
+    cardTrunfo: '',
     hasTrunfo: '',
     isSaveButtonDisabled: true,
     data: [],
